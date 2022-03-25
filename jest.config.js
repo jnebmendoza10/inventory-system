a538fd53c932874b363b581a9dcefc0860f9f560
bbmendoza <jneb_zzihw01@outlook.com>
2022-03-25 15:38:10 +0800

    Configured test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-}
+    collectCoverageFrom: ['!src/**/server.ts', 'src/**/*.ts'],
+    collectCoverage: true,
+    coverageReporters: ['json', 'json-summary', 'lcov', 'text', 'text-summary', 'html'],
+};
